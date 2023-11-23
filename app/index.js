68a6953263765b26b916d8dbc7c92ed375bbfe2b
RonaldoPHC <camposr@alunos.utfpr.edu.br>
2023-11-23 18:19:32 -0300

    Implementação do JS

---
+++ app/index.js
@@ -0,0 +1,27 @@
+class Usuario {
+    constructor(nome, email, senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+}
+
+const form = document.querySelector("form");
+const dados = document.querySelector("#dados");
+
+// Evento de submit do formulário
+// document.getElementById('form').addEventListener('submit', (event) {
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // Captura os dados do formulário
+    const nome = document.querySelector("#nome").value;
+    const email = document.querySelector("#email").value;
+    const senha = document.querySelector("#senha").value;
+
+    // Cria um objeto da classe Usuario
+    const usuario = new Usuario(nome, email, senha);
+
+    const url = "/app/perfil.html";
+    location.href = url;
+});
